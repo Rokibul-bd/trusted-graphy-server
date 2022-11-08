19a10b18c89f510a5a69cd1a424d92942a6c8f2e
Rokibul-bd <mdrokbiulislamcmt176@gmail.com>
2022-11-09 01:52:35 +0600

    create get api and get service data from database

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const port = process.env.port || 5000
 const app = express()
 
@@ -11,6 +13,36 @@ app.use(express())
 app.get('/', (req, res) => {
     res.send('your Server is Running')
 })
+
+console.log(process.env.DB_USER, process.env.DB_PASSWORD)
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0mrh6im.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const servicesCollactions = client.db('trustedGrapy').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = servicesCollactions.find(query)
+            const services = await cursor.limit(4).toArray()
+            res.send(services)
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.error(err))
+
+// client.connect(err => {
+//     const collection = client.db("test").collection("devices");
+//     // perform actions on the collection object
+//     client.close();
+// });
+
 
 
 
